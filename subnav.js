9fcaf42ad15a37a2c0c50f0572745dce67eba8f2
Charley Dupont <chly.dpnt@gmail.com>
2025-01-13 16:05:43 +0100

    Updating subnav

---
+++ subnav.js
@@ -0,0 +1,17 @@
+// Sub Nav
+
+let subNav = document.querySelector('.subnav_btn');
+let subDot = document.querySelector('.subbtn_point');
+let subText = document.querySelector('.subbtn_text');
+
+let subbtnTL = gsap.timeline({ paused: true });
+subbtnTL.to(subDot, { scale: 50, duration: 1, ease: "expo.inOut" }, 0);
+subbtnTL.to(subText, { color: "black", duration: 1, ease: "expo.inOut" }, 0);
+
+subNav.addEventListener('mouseenter', () => {
+  subbtnTL.play();
+});
+
+subNav.addEventListener('mouseleave', () => {
+  subbtnTL.reverse();
+});
