bbdabfd62b744eee6fad70eab7ab82908601fe6b
Valentin Geffroy <geffroy.valentin@gmail.com>
2025-02-13 16:06:18 +0100

    Change gamme + history card mobile

--- gamme.js
+++ gamme.js
@@ -4,82 +4,83 @@ Animation gsap des pages Gamme Performance et Architecture + effet parallax sur 
 
 // 1. Initialisation de base
 let mm = gsap.matchMedia();
-const ukiyoElements = document.querySelectorAll('.ukiyo');
+const ukiyoElements = document.querySelectorAll(".ukiyo");
 if (ukiyoElements.length > 0) {
   new Ukiyo(".ukiyo");
 }
 
 // 2. Animations Desktop (> 800px)
-mm.add("(min-width: 800px)", () => {
-  const triggers = []; // Pour le nettoyage des ScrollTriggers
+const triggers = []; // Pour le nettoyage des ScrollTriggers
 
-  // Animation du marquee
-  const sectionMarquee = document.querySelector(".marquee_gsap");
-  const marqueeWrap = document.querySelector(".espace-marquee_wrap");
-  if (marqueeWrap) {
-    triggers.push(
-      ScrollTrigger.create({
-        trigger: sectionMarquee,
-        start: "top center",
-        end: "bottom top",
-        scrub: 1,
-        // markers: true,
-        animation: gsap.fromTo(marqueeWrap,
-          {
-            xPercent: 80
-          },
-          {
-            xPercent: -80,
-            ease: "none"
-          }
-        )
-      })
-    );
-  }
+// Animation du marquee
+const sectionMarquee = document.querySelector(".marquee_gsap");
+const marqueeWrap = document.querySelector(".espace-marquee_wrap");
+if (marqueeWrap) {
+  // Vérifie si on est sur mobile
+  const isMobile = window.matchMedia("(max-width: 767px)").matches;
   
+  triggers.push(
+    ScrollTrigger.create({
+      trigger: sectionMarquee,
+      start: "top center",
+      // Fin plus tôt sur mobile
+      end: isMobile ? "bottom center" : "bottom top",
+      scrub: 1,
+      // markers: true,
+      animation: gsap.fromTo(marqueeWrap,
+        {
+          xPercent: 80
+        },
+        {
+          xPercent: -80,
+          ease: "none"
+        }
+      )
+    })
+  );
+}
 
-  // 2.1 Animation de la galerie light
-  let galleryComp = document.querySelector(".gamme-gallery_component");
-  let galleryLight = document.querySelector(".pattern-bg.is-gallery");
+// 2.1 Animation de la galerie light
+let galleryComp = document.querySelector(".gamme-gallery_component");
+let galleryLight = document.querySelector(".pattern-bg.is-gallery");
 
-  if (galleryComp && galleryLight) {
-    triggers.push(
-      ScrollTrigger.create({
-        trigger: galleryComp,
-        start: "top center",
-        toggleActions: "play reverse play reverse",
-        animation: gsap.from(galleryLight, {
-          opacity: 0,
-          duration: 0.5
-        })
-      })
-    );
-  }
+if (galleryComp && galleryLight) {
+  triggers.push(
+    ScrollTrigger.create({
+      trigger: galleryComp,
+      start: "top center",
+      toggleActions: "play reverse play reverse",
+      animation: gsap.from(galleryLight, {
+        opacity: 0,
+        duration: 0.5,
+      }),
+    })
+  );
+}
 
-  // 2.3 Animation de la sous-navigation au contact
-  let contactSection = document.querySelector(".section-global.is-contact");
-  let subNav = document.querySelector(".gamme-hero_subnav");
+// 2.3 Animation de la sous-navigation au contact
+let contactSection = document.querySelector(".section-global.is-contact");
+let subNav = document.querySelector(".gamme-hero_subnav");
 
-  if (contactSection && subNav) {
-    triggers.push(
-      ScrollTrigger.create({
-        trigger: contactSection,
-        start: "top center",
-        toggleActions: "play reverse play reverse",
-        animation: gsap.to(subNav, {
-          y: 100,
-          duration: 1,
-          ease: "power2.out"
-        })
-      })
-    );
-  }
+if (contactSection && subNav) {
+  triggers.push(
+    ScrollTrigger.create({
+      trigger: contactSection,
+      start: "top center",
+      toggleActions: "play reverse play reverse",
+      animation: gsap.to(subNav, {
+        y: 100,
+        duration: 1,
+        ease: "power2.out",
+      }),
+    })
+  );
+}
 
-  // Retourne une fonction de nettoyage
-  return () => {
-    triggers.forEach(trigger => trigger.kill());
-  };
-});
+// Fonction de nettoyage
+const cleanup = () => {
+  triggers.forEach((trigger) => trigger.kill());
+};
 
 // 3. Animation de la galerie d'images
 const nextButton = document.querySelector("#gallery-btn");
@@ -160,32 +161,32 @@ if (nextButton) {
 // Animation des éléments lumineux
 const lightTl = gsap.timeline();
 
-gsap.to('.light-on', {
+gsap.to(".light-on", {
   opacity: 1,
   duration: 3,
-  ease: 'none'
+  ease: "none",
 });
 
 // On définit d'abord l'opacité initiale à 0 pour les deux images
-gsap.set(['.is-02', '.is-03'], {
-  opacity: 0
+gsap.set([".is-02", ".is-03"], {
+  opacity: 0,
 });
 
 // On crée une timeline pour séquencer les animations
 const sequenceTl = gsap.timeline({
   defaults: {
-    duration: 3,
-    ease: 'none'
-  }
+    duration: 2,
+    ease: "none",
+  },
 });
 
 // On ajoute les animations à la timeline
 sequenceTl
-  .to('.is-02', {
-    opacity: 1
+  .to(".is-02", {
+    opacity: 1,
   })
-  .to('.is-03', {
-    opacity: 1
+  .to(".is-03", {
+    opacity: 1,
   });
 
 // Animation du titre personnalisé
@@ -194,16 +195,20 @@ const titleTl = gsap.timeline({
     trigger: ".custom-title",
     start: "center center",
     once: true,
-  }
+  },
 });
 
 titleTl
   .to(".custom-title__svg-top", {
     y: "-100%",
-    duration: 1
+    duration: 1,
   })
-  .to(".custom-title__svg-bottom", {
-    y: "0%",
-    color: "#14171A",
-    duration: 1
-  }, "<");
+  .to(
+    ".custom-title__svg-bottom",
+    {
+      y: "0%",
+      color: "#14171A",
+      duration: 1,
+    },
+    "<"
+  );
